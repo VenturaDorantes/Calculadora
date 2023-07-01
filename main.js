96270c10eab48acc2ebc7c4900936c08460ab11c
ventura <ventusDS@hotmail.com>
2023-06-30 22:06:33 -0600

    Solucionando error de los parentesis

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ for ( let key of $keys ) {
                         input.indexOf(')') != -1 &&
                         input.lastIndexOf('(') > input.lastIndexOf(')')
             ) {
-                input -= ')';
+                input += ')';
             }
             $display_input.innerHTML = input;
         } else {
